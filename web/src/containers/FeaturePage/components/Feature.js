b10e2e7ce46917349e0062da0e58b2b528b8eb41
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-09-26 20:28:13 -0300

    added style on feature page

--- web/src/containers/FeaturePage/components/Feature.js
+++ web/src/containers/FeaturePage/components/Feature.js
@@ -1,7 +1,17 @@
 import React, { memo } from 'react';
+import { Grid } from '@material-ui/core';
+
+import { styles } from './styles';
+
+const useStyles = styles;
 
 function Feature() {
-  return <div>Feature</div>;
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.container}>
+      Feature
+    </Grid>
+  );
 }
 
 export default memo(Feature);
