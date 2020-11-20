2d02f19989050d1d62b819d387920cf2dea92c98
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-11-20 17:17:41 -0300

    updated takeoffs detail page

--- web/src/containers/TakeOffDetailPage/components/TakeOffDetail.js
+++ web/src/containers/TakeOffDetailPage/components/TakeOffDetail.js
@@ -1,16 +1,42 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import { styles } from './styles'
 import ButtonIcon from 'components/Common/ButtonIcon/ButtonIcon'
 import { ArrowBack } from '@material-ui/icons'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Chip, Card, CardContent } from '@material-ui/core'
 import { history } from '../../../history'
 
 const useStyles = styles
 
 function TakeOffDetail({
   takeOff: { date, takeOffId, products, client, formOfPayment, totalPrice },
+  allProducts,
+  allClients,
+  dispatchGetProducts,
 }) {
   const classes = useStyles()
+  const selectProduct = (_id) => {
+    const [product] = allProducts.filter((product) => {
+      return product._id === _id
+    })
+    if (product) {
+      return product
+    } else {
+      return { name: 'loading...', productId: 'loading...' }
+    }
+  }
+  const selectClient = (_id) => {
+    const [client] = allClients.filter((client) => {
+      return client._id === _id
+    })
+    if (client) {
+      return client
+    } else {
+      return { name: 'loading...',socialReason: 'loading...', CPF: 'loading...', CNPJ: 'loading...', isCompany: true }
+    }
+  }
+  useEffect(() => {
+    dispatchGetProducts()
+  }, [])
   return (
     <main className={classes.content}>
       <ButtonIcon
@@ -21,26 +47,49 @@ function TakeOffDetail({
       >
         Voltar
       </ButtonIcon>
-      <Grid container alignItems="center" direction="column">
-        <Typography variant="h1">ID: {takeOffId}</Typography>
-
-        <Typography variant="h2">
-          Data da Saída: {new Date(date).toLocaleDateString()}
-        </Typography>
-        <Typography variant="h3">Produtos:</Typography>
-        {products.map(({ product, unitPrice, quantity }) => (
-          <>
-            <Typography variant="h3">ID do Produto:{product}</Typography>
-            <Typography variant="h3">Preço Unitário:{unitPrice}</Typography>
-            <Typography variant="h3">Quantidade:{quantity}</Typography>
-          </>
-        ))}
-
-        <Typography variant="h3">ID Cliente: {client}</Typography>
-        <Typography variant="h3">
-          Forma de Pagamento: {formOfPayment}
-        </Typography>
-        <Typography variant="h3">Preço Total: {totalPrice}</Typography>
+      <Grid
+        container
+        alignItems="center"
+        justify="center"
+        direction="column"
+        className={classes.container}
+      >
+        <Grid item>
+          <Chip label={`ID da saída: ${takeOffId}`} color="primary" className={classes.chips}/>
+          <Chip
+            label={`Data da Saída: ${new Date(date).toLocaleDateString()}`}
+            color="primary"
+            className={classes.chips}
+          />
+          <Typography variant="h5">
+            {products.map(({ product, unitPrice, quantity }) => (             
+                <Card className={classes.root}>
+                  <CardContent>
+                    <Typography variant="h5" component="h2">
+                      {selectProduct(product).name}
+                    </Typography>
+                    <Typography
+                      className={classes.title}
+                      gutterBottom
+                    >
+                      {selectProduct(product).productId}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      Preço Unitário: R${unitPrice}
+                      <br />
+                      Quantidade: {quantity}
+                    </Typography>
+                  </CardContent>
+                </Card>
+            ))}
+          </Typography>
+          <Chip label={`Cliente: ${selectClient(client).isCompany ? selectClient(client).socialReason : selectClient(client).name}`} color="primary" className={classes.chips}/>
+          <Chip label={selectClient(client).isCompany ? "CNPJ: " + selectClient(client).CNPJ :"CPF: " + selectClient(client).CPF} color="primary" className={classes.chips}/>
+          <br/>
+          <Chip label={`Forma de Pagamento: ${formOfPayment}`} color="primary" className={classes.chips}/>
+          <br/>
+          <Chip label={`Preço Total: R\$${totalPrice}`} color="primary" className={classes.chips}/>
+        </Grid>
       </Grid>
     </main>
   )
--- web/src/containers/TakeOffDetailPage/index.js
+++ web/src/containers/TakeOffDetailPage/index.js
@@ -3,12 +3,20 @@ import { compose } from 'redux'
 
 import TakeOffDetail from './components/TakeOffDetail'
 import { selectTakeOff } from 'containers/TakeOffsPage/meta/selectors'
+import { selectProducts } from 'containers/ProductsPage/meta/selectors'
+import { getProducts } from 'containers/ProductsPage/meta/actions'
+import { getClients } from 'containers/ClientsPage/meta/actions'
+import { selectClients } from 'containers/ClientsPage/meta/selectors'
 
 const mapStateToProps = (state, ownProps) => ({
   takeOff: selectTakeOff(state, ownProps.location.state._id),
+  allProducts: selectProducts(state),
+  allClients: selectClients(state),
 })
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = (dispatch) => ({
+  dispatchGetProducts: () => dispatch(getProducts()),
+})
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps)
 
